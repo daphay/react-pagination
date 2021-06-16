8d3c1ecd334598e1063c2ba049379c7287c2ac2c
daphay <dafizo1@gmail.com>
2021-06-16 12:33:26 +0100

    modified pagination logic to include all cases

--- src/usePagination.js
+++ src/usePagination.js
@@ -21,7 +21,31 @@ const usePagination = ({
   
      //show dot logic
      const shouldShowLeftDots = leftSiblingIndex > 2
-     const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2
+     const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+
+     const firstPageIndex = 1;
+     const lastPageIndex = totalPageCount;
+
+     //where right dots exist
+     if(!shouldShowLeftDots && shouldShowRightDots){
+         let leftItemCount = 3 + 2 * siblingCount;
+         let leftRange = range (1, leftItemCount);
+         return[...leftRange, DOTS, totalPageCount];
+     }
+
+     //where left dots exist
+     if(shouldShowLeftDots && !shouldShowRightDots){
+        let rightItemCount = 3 + 2 * siblingCount;
+        let RightRange = range (totalPageCount - rightItemCount, totalPageCount);
+        return[firstPageIndex, DOTS, ...RightRange];
+    }
+
+    //where both left and right dots exist
+    if(shouldShowLeftDots && shouldShowRightDots){
+        let middleRange = range (leftSiblingIndex, rightSiblingIndex);
+        return[firstPageIndex, DOTS, ...middleRange, lastPageIndex];
+    }
+
      //page Range
      const range = (start, end) => {
          let length = end - start + 1;
