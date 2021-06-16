ec4c8aac632c4dd0c60d9e73cb90bfa47d3b9ed6
daphay <dafizo1@gmail.com>
2021-06-16 12:07:12 +0100

    added pagination logic

--- src/usePagination.js
+++ src/usePagination.js
@@ -7,7 +7,26 @@ const usePagination = ({
     currentPage
 }) => {
    const  paginationRange = useMemo(() => {
+     const totalPageCount = Math.ceil(totalCount/pageSize);
+     const totalPageNumber = siblingCount + 5;
 
+   //where page count is less that page number shown 
+     if(totalPageNumber >= totalPageCount){
+         return range(1, totalPageCount);
+     }
+
+     //left and right sibling calc(must be btw 1 and totalPageCount)
+     const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
+     const rightSiblingIndex = Math.max(currentPage + siblingCount, totalPageCount);
+  
+     //show dot logic
+     const shouldShowLeftDots = leftSiblingIndex > 2
+     const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2
+     //page Range
+     const range = (start, end) => {
+         let length = end - start + 1;
+         return Array.from({length}, (_,idx) => idx + start);
+     };
    },[totalCount, pageSize, siblingCount, currentPage]);
 
    return paginationRange
