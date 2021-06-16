f79a66bfd1b7fddc2a2b78a76acf987fb2d2a847
daphay <dafizo1@gmail.com>
2021-06-16 19:24:31 +0100

    modified App component to display mock data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import data from './data/mock-data.json';
 
 export default function App() {
-    return (
-        <div>
-            <h1>Hello just checking my pulse</h1>
-        </div>
-    )
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>FIRST NAME</th>
+            <th>LAST NAME</th>
+            <th>EMAIL</th>
+            <th>PHONE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
